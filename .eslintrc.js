10d8d9b2d4833688a82356a75b3249b01198e85c
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-08-03 19:24:10 -0300

    chore(eslint): adds rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: ['universe/native'],
+  rules: {
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: 'error',
+    'max-len': ['error', { code: 120, tabWidth: 2 }],
+  },
 };
