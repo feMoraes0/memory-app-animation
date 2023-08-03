085a65c5eeec189c95e6c77da33b6d1b3e890840
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-08-02 22:45:03 -0300

    refactor(main): updates container with start and resize animations

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,56 @@
-import { ImageSourcePropType } from 'react-native';
+import { useEffect } from 'react';
+import { Animated, ImageSourcePropType, Dimensions } from 'react-native';
 
 import { ImageBackground, Container } from './styles';
 import MainBackgroundImage from '../../images/main-background.png';
 
+const { height: kScreenHeight, width: kScreenWidth } = Dimensions.get('screen');
+const kInitialContainerSize = 100;
+
 const Main = () => {
+  const translateY = new Animated.Value(0);
+  const width = new Animated.Value(kInitialContainerSize);
+  const height = new Animated.Value(kInitialContainerSize);
+
+  useEffect(() => {
+    const parallelDuration = 800;
+
+    Animated.sequence([
+      Animated.spring(translateY, {
+        toValue: (kScreenHeight - kInitialContainerSize) / 2,
+        bounciness: 16,
+        useNativeDriver: false,
+      }),
+
+      Animated.parallel([
+        Animated.timing(height, {
+          toValue: kScreenHeight,
+          duration: parallelDuration,
+          useNativeDriver: false,
+        }),
+        Animated.timing(width, {
+          toValue: kScreenWidth,
+          duration: parallelDuration,
+          useNativeDriver: false,
+        }),
+        Animated.timing(translateY, {
+          toValue: 0,
+          duration: parallelDuration,
+          useNativeDriver: false,
+        }),
+      ]),
+    ]).start();
+  }, []);
+
   return (
     <ImageBackground source={MainBackgroundImage as ImageSourcePropType} resizeMode="cover">
-      <Container />
+      <Container
+        style={{
+          width,
+          height,
+          transform: [{ translateY }],
+        }}
+      />
     </ImageBackground>
   );
 };
--- src/pages/main/styles.ts
+++ src/pages/main/styles.ts
@@ -1,11 +1,14 @@
+import { Animated } from 'react-native';
 import styled from 'styled-components/native';
 
 const ImageBackground = styled.ImageBackground`
   flex: 1;
 `;
 
-const Container = styled.View`
-  flex: 1;
+const Container = styled(Animated.View)`
+  align-self: center;
+  background-color: #fff;
+  border-radius: 10px;
 `;
 
 export { ImageBackground, Container };
