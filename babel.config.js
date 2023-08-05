2e59f7ffe97875b141234fa9381cce987ffd064d
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-08-05 19:37:46 -0300

    build(alias): adds alias config and additional configs to work

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@pages': './src/pages',
+            '@assets': './src/assets',
+          },
+        },
+      ],
+    ],
   };
 };
